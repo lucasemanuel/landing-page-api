70b4f09dfceb14e82fab643b3afe232004940812
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-19 11:56:52 -0300

    test: ensure ContactEntity contain id

--- src/domain/entities/ContactEntity.spec.ts
+++ src/domain/entities/ContactEntity.spec.ts
@@ -1,11 +1,22 @@
 class ContactEntity {
-  constructor (public readonly name: string, public readonly email: string) {}
+  constructor (
+    public readonly name: string,
+    public readonly email: string,
+    public readonly id: string
+  ) {}
 }
 
 describe('Contact Entity', () => {
-  test('should contain correct name and email', () => {
-    const sut = new ContactEntity('any_name', 'any_email@email.com')
+  test('should contain correct name', () => {
+    const sut = new ContactEntity('any_name', 'any_email@email.com', 'any_id')
     expect(sut.name).toBe('any_name')
+  })
+  test('should contain correct email', () => {
+    const sut = new ContactEntity('any_name', 'any_email@email.com', 'any_id')
     expect(sut.email).toBe('any_email@email.com')
+  })
+  test('should contain correct email', () => {
+    const sut = new ContactEntity('any_name', 'any_email@email.com', 'any_id')
+    expect(sut.id).toBe('any_id')
   })
 })
