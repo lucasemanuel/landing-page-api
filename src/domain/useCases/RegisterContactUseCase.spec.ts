239504c12d552e53d1bfd0923b7c4c16c2d2b44e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-19 17:05:24 -0300

    test: ensure return a ContactEntity

--- src/domain/useCases/RegisterContactUseCase.spec.ts
+++ src/domain/useCases/RegisterContactUseCase.spec.ts
@@ -1,3 +1,5 @@
+import ContactEntity from '../entities/ContactEntity'
+
 interface RegisterContactDTO {
   name: string
   email: string
@@ -6,33 +8,53 @@ interface RegisterContactDTO {
 class RegisterContactUseCase {
   constructor (private readonly contactRepository: ContactRepository) {}
 
-  public async execute (data: RegisterContactDTO): Promise<void> {
-    await this.contactRepository.insert(data)
+  public async execute (
+    data: RegisterContactDTO
+  ): Promise<ContactEntity | null> {
+    const contact = await this.contactRepository.insert(data)
+    return contact
   }
 }
 
 interface ContactRepository {
-  insert: (data: RegisterContactDTO) => Promise<void>
+  insert: (data: RegisterContactDTO) => Promise<ContactEntity>
 }
 
 class ContactRepositorySpy implements ContactRepository {
   public name: string
   public email: string
 
-  public async insert (data: RegisterContactDTO): Promise<void> {
+  public async insert (data: RegisterContactDTO): Promise<ContactEntity> {
     Object.assign(this, data)
+    return new ContactEntity('any_name', 'any_email@email.com', 'any_id')
+  }
+}
+
+const makeSut = (): any => {
+  const contactRepositorySpy = new ContactRepositorySpy()
+  const sut = new RegisterContactUseCase(contactRepositorySpy)
+  return {
+    sut,
+    contactRepositorySpy
   }
 }
 
 describe('Register Contact Use Case', () => {
   test('should call ContactRepository with correct params', async () => {
-    const contactRepositorySpy = new ContactRepositorySpy()
-    const sut = new RegisterContactUseCase(contactRepositorySpy)
+    const { contactRepositorySpy, sut } = makeSut()
     await sut.execute({
       name: 'any_name',
       email: 'any_email@email.com'
     })
     expect(contactRepositorySpy.name).toBe('any_name')
     expect(contactRepositorySpy.email).toBe('any_email@email.com')
+  })
+  test('should return a ContactEntity', async () => {
+    const { sut } = makeSut()
+    const contact = await sut.execute({
+      name: 'any_name',
+      email: 'any_email@email.com'
+    })
+    expect(contact).toBeInstanceOf(ContactEntity)
   })
 })
