df91716ce794de72413a423508f293896061c112
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-21 16:51:25 -0300

    refactor: move MissingParamError to its own file

---
+++ src/helpers/errors/MissingParamError.ts
@@ -0,0 +1,8 @@
+class MissingParamError extends Error {
+  constructor (param: string) {
+    super(`Missing param: ${param}`)
+    this.name = 'MissingParamError'
+  }
+}
+
+export default MissingParamError
