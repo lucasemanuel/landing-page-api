22968d75c01442e484cca8dbb5a4e00118499e16
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-22 11:52:50 -0300

    chore: register contact use case receive a contact DTO with param

--- src/domain/useCases/RegisterContactUseCase.ts
+++ src/domain/useCases/RegisterContactUseCase.ts
@@ -1,10 +1,13 @@
 import ContactEntity from '../entities/ContactEntity'
 import ContactRepository from '../repositories/ContactRepository'
+import CreateContactDTO from '../../dtos/CreateContactDTO'
+import ContactFactory from '../../factories/ContactFactory'
 
 class RegisterContactUseCase {
   constructor (private readonly contactRepository: ContactRepository) {}
 
-  public async execute (contact: ContactEntity): Promise<ContactEntity> {
+  public async execute (data: CreateContactDTO): Promise<ContactEntity> {
+    const contact = ContactFactory.create(data)
     await this.contactRepository.insert(contact)
     return contact
   }
