437de2616d3cad5596a21bb8fb163baf82a4acc1
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-22 11:55:13 -0300

    fix: condition to create a contact

--- src/presentation/controllers/RegisterContactController.ts
+++ src/presentation/controllers/RegisterContactController.ts
@@ -1,9 +1,10 @@
+import ContactEntity from '../../domain/entities/ContactEntity'
 import GetContactByEmailUseCase from '../../domain/useCases/GetContactByEmailUseCase'
 import HttpRequest from '../helpers/HttpRequest'
 import HttpResponse from '../helpers/HttpResponse'
 import MissingParamError from '../../helpers/errors/MissingParamError'
 import RegisterContactUseCase from '../../domain/useCases/RegisterContactUseCase'
-import { BadRequest, Created } from '../helpers/responses'
+import { BadRequest, Created, InternalServerError } from '../helpers/responses'
 
 class RegisterContactController {
   constructor (
@@ -12,21 +13,26 @@ class RegisterContactController {
   ) {}
 
   public async route (request: HttpRequestExtends): Promise<HttpResponse> {
-    const { name, email } = request.body
-    if (name.length !== 0) {
-      return BadRequest(new MissingParamError('name').message)
+    try {
+      const { name = '', email = '' } = request.body
+      if (name.length === 0) {
+        return BadRequest(new MissingParamError('name').message)
+      }
+      if (email.length === 0) {
+        return BadRequest(new MissingParamError('email').message)
+      }
+      const existsContact = await this.getContactByEmailUseCase.execute(email)
+      if (!(existsContact instanceof ContactEntity)) {
+        const contact = await this.registerContactUseCase.execute({
+          name,
+          email
+        })
+        return Created(contact)
+      }
+      return BadRequest('This email has already been registered.')
+    } catch (error) {
+      return InternalServerError()
     }
-    if (email.length !== 0) {
-      return BadRequest(new MissingParamError('email').message)
-    }
-
-    let contact = await this.getContactByEmailUseCase.execute(email)
-    if (contact !== null) {
-      contact = await this.registerContactUseCase.execute(contact)
-      return Created(contact)
-    }
-
-    return BadRequest('This email has already been registered.')
   }
 }
 
