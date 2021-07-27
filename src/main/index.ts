16f68de8ed0234cf356834a5cc4d14c36f578c67
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-27 11:58:01 -0300

    feat: starter queue

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,16 @@
 import app from '../infra/web/configs/App'
 import { database } from '../infra/database/MongoDB'
 import { MONGO_URI } from '../infra/config/Env'
+import queue from '../infra/queue/EmailQueueBull'
 
 async function main (): Promise<any> {
   await database.connect(MONGO_URI)
   app.listen(3333, () => console.log('Server is Running!'))
+
+  queue.on('failed', (job, err) => {
+    console.log('Job failed', queue.name, job.data)
+    console.log(err)
+  })
 }
 
 main().catch(console.error)
