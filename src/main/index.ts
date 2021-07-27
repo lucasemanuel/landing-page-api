933dec2689baf897fc9970e36116b12ca0af4c5c
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-27 17:11:37 -0300

    refactor: set env variables in uri mongo

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,17 @@
 import app from '../infra/web/configs/App'
-import { database } from '../infra/database/MongoDB'
-import { MONGO_URI } from '../infra/config/Env'
 import queue from '../infra/queue/EmailQueueBull'
+import { database } from '../infra/database/MongoDB'
+import {
+  MONGO_HOST,
+  MONGO_PASSWORD,
+  MONGO_PORT,
+  MONGO_USER
+} from '../infra/config/Env'
 
 async function main (): Promise<any> {
-  await database.connect(MONGO_URI)
+  await database.connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}`
+  )
   app.listen(3333, () => console.log('Server is Running!'))
 
   queue.on('failed', (job, err) => {
