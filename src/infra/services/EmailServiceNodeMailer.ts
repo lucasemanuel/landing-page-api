3c53b3910ce31402c1c53837c71b4747c33ac809
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-07-27 11:57:21 -0300

    feat: add email in queue

--- src/infra/services/EmailServiceNodeMailer.ts
+++ src/infra/services/EmailServiceNodeMailer.ts
@@ -1,19 +1,15 @@
 import EmailEntity from '../../domain/entities/EmailEntity'
 import EmailService from '../../domain/services/EmailService'
-import Mailer from '../config/Mailer'
-import { MAIL_FROM_EMAIL, MAIL_FROM_NAME } from '../config/Env'
+import queue from '../queue/EmailQueueBull'
+import { Queue } from 'bull'
 
 class EmailServiceNodeMailer implements EmailService {
+  private readonly queue: Queue = queue
+
   constructor (private readonly email: EmailEntity) {}
 
   public async send (emailTo: string): Promise<void> {
-    const { body, subject } = this.email
-    await Mailer.sendMail({
-      from: `"${MAIL_FROM_EMAIL}" <${MAIL_FROM_NAME}>`,
-      to: [emailTo],
-      subject,
-      text: body.text
-    })
+    await this.queue.add({ emailTo, email: this.email }, { delay: 30000 })
   }
 }
 
